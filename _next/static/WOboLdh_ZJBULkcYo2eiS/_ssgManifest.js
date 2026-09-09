97471b3ab7548dc8719ba2939eb2c686bc25f754
charliefei <2062753446@qq.com>
2026-09-09 11:05:59 +0800

    deploy

---
+++ _next/static/WOboLdh_ZJBULkcYo2eiS/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fexperience","\u002F[locale]\u002Fresume","\u002F[locale]\u002Frss.xml"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
